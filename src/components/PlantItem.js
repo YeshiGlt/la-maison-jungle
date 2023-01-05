2bb5bef3d95af40e608098b634f53225c72a736d
YeshiGlt <39733077+YeshiGlt@users.noreply.github.com>
2023-01-05 23:54:34 +0100

    ‼️ BREAKING: Découverte des évènements synthétiques

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -3,7 +3,7 @@ import CareScale from "./CareScale";
 
 function PlantItem({ id, cover, name, water, light }) {
   return (
-    <li key={id} className="lmj-plant-item" onClick={() => handleClick(name)}>
+    <li key={id} className="lmj-plant-item" onClick={handleClick}>
       <img src={cover} alt={`${name} cover`} className="lmj-plant-item-cover" />
       {name}
       <div>
@@ -14,8 +14,8 @@ function PlantItem({ id, cover, name, water, light }) {
   );
 }
 
-function handleClick(plantName) {
-  alert(`Vous voulez acheter 1 ${plantName} ? Très bon choix 🌱✨`);
+function handleClick(e) {
+  console.log("✨ Ceci est mon event", e);
 }
 
 export default PlantItem;
