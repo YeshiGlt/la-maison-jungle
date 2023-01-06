7b7d1a6f662c3594dd9d5665d845c53902aad8a6
YeshiGlt <39733077+YeshiGlt@users.noreply.github.com>
2023-01-06 23:42:44 +0100

    📦 NEW: Découverte des avantages des formulaires contrôlés

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,15 +1,23 @@
-function QuestionForm() {
-  return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" name="my_input" defaultValue="Tapez votre texte" />
-      <button type="submit">Entrer</button>
-    </form>
-  );
-}
+import { useState } from "react";
 
-function handleSubmit(e) {
-  e.preventDefault();
-  alert(e.target["my_input"].value);
+function QuestionForm() {
+  const [inputValue, setInputValue] = useState("Posez votre question ici");
+
+  const isInputError = inputValue.includes("f");
+
+  return (
+    <div>
+      <textarea
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={() => alert(inputValue)}>Alertez moi 🚨</button>
+
+      {isInputError && (
+        <div>🔥 Vous n'avez pas le droit d'utiliser la lettre "f" ici.</div>
+      )}
+    </div>
+  );
 }
 
 export default QuestionForm;
