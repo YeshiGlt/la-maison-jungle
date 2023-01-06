3902e025c195792b9874ba55ef3136293e2ad728
YeshiGlt <39733077+YeshiGlt@users.noreply.github.com>
2023-01-06 16:15:32 +0100

    📦 NEW: Création d'un formulaire non contrôlé (un input + un button afin d'y découvrir brièvement)

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Banner from "./Banner";
 import Cart from "./Cart";
 import ShoppingList from "./ShoppingList";
+import QuestionForm from "./QuestionForm";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Banner />
       <Cart />
       <ShoppingList />
+      <QuestionForm />
     </React.Fragment>
   );
 }
