3742cc24f23464f278d0d98f12c52dcd5bb6012b
YeshiGlt <39733077+YeshiGlt@users.noreply.github.com>
2023-01-10 14:08:50 +0100

    📦 NEW: Afficher une div "Soldes" selon une condition (isSpecialOffer true) + ajout de style

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,5 @@
 import { plantList } from "../datas/plantList";
+import "../styles/ShoppingList.css";
 
 function ShoppingList() {
   const categoriesPlant = plantList.reduce(
@@ -9,16 +10,16 @@ function ShoppingList() {
 
   return (
     <div>
-      <ul>
+      <ul className="lmj-plant-list">
         {categoriesPlant.map((cat) => (
           <li key={cat}>{cat}</li>
         ))}
       </ul>
       <ul>
         {plantList.map((plant) => (
-          <li key={plant.id}>
+          <li key={plant.id} className="lmj-plant-item">
             {plant.name}
-            {plant.isBestSale && <span>🔥</span>}
+            {plant.isSpecialOffer && <div className="lmj-sales">Soldes</div>}
           </li>
         ))}
       </ul>
--- src/datas/plantList.js
+++ src/datas/plantList.js
@@ -1,12 +1,11 @@
 export const plantList = [
-  { id: "1ab", name: "monstera", category: "intérieur", isBestSale: true },
-  { id: "2cd", name: "ficus lyrata", category: "extérieur", isBestSale: false },
+  { id: "1ab", name: "monstera", category: "intérieur", isSpecialOffer: true },
+  { id: "2cd", name: "ficus lyrata", category: "extérieur" },
   {
     id: "3ef",
     name: "pothos argenté",
     category: "extérieur",
-    isBestSale: false,
   },
-  { id: "4gh", name: "yucca", category: "intérieur", isBestSale: true },
-  { id: "5ij", name: "palmier", category: "extérieur", isBestSale: true },
+  { id: "4gh", name: "yucca", category: "intérieur", isSpecialOffer: true },
+  { id: "5ij", name: "palmier", category: "extérieur", isSpecialOffer: true },
 ];
