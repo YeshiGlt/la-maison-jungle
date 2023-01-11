418a3d1dbdbd8571368aeba78681787d2a5f468f
YeshiGlt <39733077+YeshiGlt@users.noreply.github.com>
2023-01-11 04:18:17 +0100

    🚀 RELEASE: Créer une alert pour afficher la quantité d'eau ou d'arrosage dynamiquement (au click)

--- src/components/CareScale.js
+++ src/components/CareScale.js
@@ -4,6 +4,12 @@ import Water from "../assets/water.svg";
 function CareScale({ scaleValue, careType }) {
   const range = [1, 2, 3];
 
+  const quantityLW = {
+    1: "peu",
+    2: "modérement",
+    3: "beaucoup",
+  };
+
   const scaleType =
     careType === "light" ? (
       <img src={Light} alt="light-icon" />
@@ -11,11 +17,24 @@ function CareScale({ scaleValue, careType }) {
       <img src={Water} alt="water-icon" />
     );
 
+  function handleClick({ quantityLW, scaleValue, careType }) {
+    alert(
+      `Cette plante requiert ${quantityLW[scaleValue]} ${
+        careType === "light" ? "de lumière" : "d'arrosage"
+      }`
+    );
+  }
+
   return (
     <div>
       {range.map((rangeElem) =>
         scaleValue >= rangeElem ? (
-          <span key={rangeElem.toString()}>{scaleType}</span>
+          <span
+            key={rangeElem.toString()}
+            onClick={() => handleClick({ quantityLW, scaleValue, careType })}
+          >
+            {scaleType}
+          </span>
         ) : null
       )}
     </div>
