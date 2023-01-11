bd60d661a6be056404edd06329c4539530f5815d
YeshiGlt <39733077+YeshiGlt@users.noreply.github.com>
2023-01-11 23:18:32 +0100

    🚀 RELEASE: Reprise exo : Récupérer le mail d'un user avec un form contrôlé + vérifier sa valeur

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Banner from "./Banner";
 import Cart from "./Cart";
 import ShoppingList from "./ShoppingList";
+import QuestionForm from "./QuestionForm";
+import Footer from "./Footer";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Banner />
       <Cart />
       <ShoppingList />
+      <QuestionForm />
+      <Footer />
     </React.Fragment>
   );
 }
